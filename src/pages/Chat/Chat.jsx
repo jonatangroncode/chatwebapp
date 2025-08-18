58c34952ce36205dea45a7f967911de6e1fcfdb4
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-08-18 13:41:20 +0200

    Get and desplay messages

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Chat.css";
 
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        const res = await fetch("https://chatify-api.up.railway.app/messages", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${sessionStorage.getItem("jwt_token")}`,
+          },
+        });
+
+        if (!res.ok) {
+          throw new Error("Misslyckades att hämta meddelanden");
+        }
+
+        const data = await res.json();
+        setMessages(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchMessages();
+  }, []);
+
+  console.log(messages);
   return (
     <div className="chat-page">
       <header className="chat-header">Chat</header>
       <div className="chat-container">
         <div className="chat-messages">
-          <div className="message">Hello!</div>
-          <div className="message">Hi there!</div>
+          {messages.map((msg) => (
+            <div key={msg.id} className="chat-message">
+              <p className="message-text">{msg.text}</p>
+            </div>
+          ))}
         </div>
         <input type="text" />
         <button type="submit">Send</button>
