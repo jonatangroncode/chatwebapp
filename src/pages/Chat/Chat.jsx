f1c94861ba8f6e1760891c5eab0a0732e6ffde18
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-08-18 13:08:53 +0200

    Starting chat elements

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const Chat = () => {
   return (
-    <div>
-      <h1>Chat Page!!</h1>
+    <div className="chat-page">
+      <header className="chat-header">Chat</header>
+      <div className="chat-container">
+        <div className="chat-messages">
+          <div className="message">Hello!</div>
+          <div className="message">Hi there!</div>
+        </div>
+        <input type="text" />
+        <button type="submit">Send</button>
+      </div>
     </div>
   );
 };
